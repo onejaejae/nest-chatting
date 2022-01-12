c0857cfc5c0c9efaae4bd84de1f1a08435245d1d
Cho Won Jae <yoteamo7@naver.com>
2022-01-13 00:12:28 +0900

    네임스페이스의 이해와 Gateway 생명주기

--- chattings/src/chats/chats.gateway.ts
+++ chattings/src/chats/chats.gateway.ts
@@ -1,13 +1,33 @@
+import { Logger } from '@nestjs/common';
 import {
   SubscribeMessage,
   WebSocketGateway,
   MessageBody,
   ConnectedSocket,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatsGateway {
+@WebSocketGateway({ namespace: 'chattings' })
+export class ChatsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger = new Logger('chat');
+
+  handleDisconnect() {
+    this.logger.log('Disconnection');
+  }
+
+  handleConnection() {
+    this.logger.log('connection');
+  }
+
+  afterInit() {
+    this.logger.log('init');
+  }
+
   @SubscribeMessage('new_user')
   handleNewUser(
     @MessageBody() username: string,
