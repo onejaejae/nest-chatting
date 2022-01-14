ca4e9976da1f50a12d2ee50a0d74127bec1c0040
Cho Won Jae <yoteamo7@naver.com>
2022-01-14 13:52:13 +0900

    디자인 입히기 with TailwindCSS & 프로젝트 마무리

--- chattings/public/js/scripts.js
+++ chattings/public/js/scripts.js
@@ -2,12 +2,12 @@ const socket = io('/chattings');
 
 const getElementById = (id) => document.getElementById(id) || null;
 
-// get DOM element
+//* get DOM element
 const helloStrangerElement = getElementById('hello_stranger');
 const chattingBoxElement = getElementById('chatting_box');
 const formElement = getElementById('chat_form');
 
-// global socket handler
+//* global socket handler
 socket.on('user_connected', (username) => {
   drawNewChat(`${username} connected!`);
 });
@@ -17,28 +17,37 @@ socket.on('new_chat', (data) => {
 });
 socket.on('disconnect_user', (username) => drawNewChat(`${username}: bye...`));
 
-// event callback functions
+//* event callback functions
 const handleSubmit = (event) => {
   event.preventDefault();
   const inputValue = event.target.elements[0].value;
-
   if (inputValue !== '') {
     socket.emit('submit_chat', inputValue);
-
     // 화면에다가 그리기
-    drawNewChat(`me: ${inputValue}`);
+    drawNewChat(`me : ${inputValue}`, true);
     event.target.elements[0].value = '';
   }
 };
 
-// draw functions
-const drawHelloStranger = (username) => {
-  helloStrangerElement.innerText = `Hello ${username} :)`;
-};
-const drawNewChat = (message) => {
+//* draw functions
+const drawHelloStranger = (username) =>
+  (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
+const drawNewChat = (message, isMe = false) => {
   const wrapperChatBox = document.createElement('div');
-  const chatBox = `<div>${message}</div>`;
-
+  wrapperChatBox.className = 'clearfix';
+  let chatBox;
+  if (!isMe)
+    chatBox = `
+    <div class='bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
+    `;
+  else
+    chatBox = `
+    <div class='bg-white w-3/4 ml-auto mr-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
+    `;
   wrapperChatBox.innerHTML = chatBox;
   chattingBoxElement.append(wrapperChatBox);
 };
@@ -52,6 +61,7 @@ function helloUser() {
 
 function init() {
   helloUser();
+  // 이벤트 연결
   formElement.addEventListener('submit', handleSubmit);
 }
 
